8ccadb5c8dc065e0cd94a7ccaf458038789c1e85
tash-ma-gosh <n.karnoto@gmail.com>
2023-04-21 01:09:15 -0400

    initial

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+  return <div>Hello World</div>;
+  // return (
+  //   <div className='home'>
+  
+  //     <div className='headerContainer'>
+  //       <h1>Natasha Karnoto</h1>
+  //       <p>UI/UX Designer | Web/Software Developer | Llama Lover </p>
+  //     </div>
+  //   </div>
+  // );
+}
+
+export default Home;
