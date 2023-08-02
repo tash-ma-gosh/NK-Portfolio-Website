cdcfc6da6b289b253b6ae84867be5341fd7c77e6
tash-ma-gosh <n.karnoto@gmail.com>
2023-08-02 00:12:56 -0400

    Format top section of project page

--- src/components/PageTemplate.js
+++ src/components/PageTemplate.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import FillerImage from '../assets/art.png'
 
 function PageTemplate({content}) {
   return (
     <div className='page'>
+      <div className='top-section'>
+        <h1>Title of Project</h1>
+        <p>Have one to three sentences describing the 
+          product and its purpose.</p>
+        <img src={FillerImage} alt='Do not forget to include'></img>
+      </div>
+      <div className='middle-section'>
+        <div className='middle-container'>
+          <div className='goal'>
+            <h2>Goal</h2>
+            <p>Have one to three sentences describing the goal of the product</p>
+          </div>
+          <div className='role'>
+            <h2>My Role</h2>
+            <p>Software Developer, UI UX, Juggling</p>
+          </div>
+          <div className='tools'>
+            <h2>Tools</h2>
+            <p>Laptop, Pure Skill, and Maple Syrup</p>
+          </div>
+        </div>
+      </div>
+      <div className='bottom-section'>
+        <h2>More Context</h2>
+        {/* Do a similar method to projects overview pageS */}
+        <div className='indv-item'>
+          <img src={FillerImage} alt='Do not forget to include'></img>
+          <p>A five word blurb description</p>
+        </div>
+        <div className='indv-item'>
+          <img src={FillerImage} alt='Do not forget to include'></img>
+          <p>A five word blurb description</p>
+        </div>
+        <div className='indv-item'>
+          <img src={FillerImage} alt='Do not forget to include'></img>
+          <p>A five word blurb description</p>
+        </div>
+        <div className='indv-item'>
+          <img src={FillerImage} alt='Do not forget to include'></img>
+          <p>A five word blurb description</p>
+        </div>
+
+      </div>
+      
         <p>{content}</p>
 
     </div>
--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -8,28 +8,24 @@ export const ProjectList = [
         image: DiatomPic,
         // skills: 'insert catchy description woohoo',
         skills: ["C#", "User Research", "UX Design", "Touchdesigner"],
-        href: "https://github.com/tash-ma-gosh"
     },
     {
         id: "portfolio",
         name: "Front-End Portfolio Development",
         image: ImagePlaceholder,
         skills: ["React", "UI/UX Design"],
-        href: "https://github.com/tash-ma-gosh"
     },
     {
         id: 3,
         name: "Case Study: Website Redesign",
         image: ImagePlaceholder,
         skills: ["Figma", "User Research", "UX Design"],
-        href: "https://github.com/tash-ma-gosh"
     },
     {
         id: 4,
         name: "Sofia's Forest",
         image: ImagePlaceholder,
         skills: ["Python", "OSC", "Arduino"],
-        href: "https://github.com/tash-ma-gosh"
     },
     
     {
@@ -37,7 +33,6 @@ export const ProjectList = [
         name: "Climate Impact Competition",
         image: ImagePlaceholder,
         skills: ["HTML/CSS", "User Research"],
-        href: "https://github.com/tash-ma-gosh"
     },
     
 ]
